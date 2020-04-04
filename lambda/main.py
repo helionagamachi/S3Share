c3d464d3edd95eaaa0e23023e7ce005e24c72cb2
Hélio Nagamachi <helionagamachi@users.noreply.github.com>
2020-04-04 09:52:15 -0300

    Some refactor and other stuff
    - Add some deletion policies to preserve the lambda function and
    some of the related resources. Since we are deploy to @edge it is
    not possible to remove the function or the version for some time
    after the Cloudfront distribution is removed.

--- lambda/main.py
+++ lambda/main.py
@@ -1,17 +1,16 @@
-import jwt
 from datetime import datetime, timedelta
+
+import jwt
 
 key = "#KEY_TO_BE_REPLACED#"
 
 def handler(event, context):
-
     if event.get('Records') is not None:
         return process_cf_request(event)
 
     return generate_token(event)
 
 def generate_token(event):
-
     uri = event['uri']
 
     if uri[0] is not '/':
@@ -31,13 +30,13 @@ def process_cf_request(event):
     try:
         # The uri will start with a "/" that is not part of the token that
         # we are trying to decode
-        decoded = jwt.decode(request["uri"][1:], key, algorithms=['HS256'])
+        decoded = jwt.decode(request["uri"][1:], key, algorithms=["HS256"])
         request["uri"] = decoded["uri"]
         return request
     except jwt.PyJWTError:
         return {
-            "status": "401",
-            "statusDescription": "Unauthorized",
-            "body": "Unauthorized",
-            "bodyEncoding": "text"
+            'status': "401",
+            'statusDescription': "Unauthorized",
+            'body': "Unauthorized",
+            'bodyEncoding': "text"
         }
--- s3_share/s3_share_stack.py
+++ s3_share/s3_share_stack.py
@@ -1,3 +1,12 @@
+import tempfile
+import shutil
+import string
+import random
+import os
+
+from os import environ
+from pathlib import Path
+
 from aws_cdk import core
 
 from aws_cdk.aws_cloudfront import (
@@ -5,40 +14,18 @@ from aws_cdk.aws_cloudfront import (
     OriginAccessIdentity, LambdaFunctionAssociation, LambdaEdgeEventType
 )
 
-from aws_cdk.aws_s3 import Bucket
-from aws_cdk.aws_s3 import BlockPublicAccess
+from aws_cdk.aws_s3 import Bucket, BlockPublicAccess
 
 from aws_cdk.aws_lambda import Function, Code, Runtime, Version
 
 from aws_cdk.aws_iam import ServicePrincipal, PolicyStatement
 
-from os import environ
-from pathlib import Path
-import tempfile, shutil, string, random, os
 
 class S3ShareStack(core.Stack):
 
     def init_lambda(self):
 
-        tmp_dir = Path(tempfile.mkdtemp())
-        origin_dir = Path(__file__).parent.parent.joinpath("lambda")
-
-        shutil.copy(origin_dir.joinpath("requirements.txt"), tmp_dir)
-
-        random_key = ''.join(
-            random.choices(string.printable, k=30)
-        ).replace('"', "").replace("\\", "")
-
-        key = environ.get("JWT_KEY", random_key)
-
-        with open(tmp_dir.joinpath("main.py"), "w") as dest:
-            with open (origin_dir.joinpath("main.py")) as original:
-                for l in original:
-                    dest.write(l.replace("#KEY_TO_BE_REPLACED#", key))
-
-        # Installing the dependencies, maybe could be done better?
-        os.chdir(tmp_dir)
-        os.system("pip install -r requirements.txt --target .")
+        tmp_dir = install_lambda_code_requirements()
 
         lambda_code = Code.from_asset(
             str(tmp_dir),
@@ -51,7 +38,7 @@ class S3ShareStack(core.Stack):
             ]
         )
 
-        checker_lambda = Function(
+        lambda_function = Function(
             self,
             "lambda",
             code=lambda_code,
@@ -59,15 +46,16 @@ class S3ShareStack(core.Stack):
             runtime=Runtime.PYTHON_3_8
         )
 
-        checker_lambda.role.assume_role_policy.add_statements(
+        lambda_function.role.assume_role_policy.add_statements(
             PolicyStatement(
                 actions=["sts:AssumeRole"],
                 principals=[ServicePrincipal("edgelambda.amazonaws.com")]
             )
         )
 
+        version = Version(self, "version", lambda_=lambda_function)
 
-        version = Version(self, "version", lambda_=checker_lambda)
+        apply_removal_policy(lambda_function, version, lambda_function.role)
 
         return version
 
@@ -90,6 +78,8 @@ class S3ShareStack(core.Stack):
         )
 
         bucket.grant_read(identity)
+
+        apply_removal_policy(bucket)
 
         default_behavior = Behavior(
             is_default_behavior=True,
@@ -116,3 +106,31 @@ class S3ShareStack(core.Stack):
                 source_config,
             ],
         )
+
+def apply_removal_policy(*args):
+    for el in args:
+        resource = el.node.find_child('Resource')
+        resource.apply_removal_policy(core.RemovalPolicy.RETAIN)
+
+def install_lambda_code_requirements():
+    tmp_dir = Path(tempfile.mkdtemp())
+    origin_dir = Path(__file__).parent.parent.joinpath("lambda")
+
+    shutil.copy(origin_dir.joinpath("requirements.txt"), tmp_dir)
+
+    random_key = ''.join(
+        random.choices(string.printable, k=30)
+    ).replace('"', "").replace("\\", "")
+
+    key = environ.get("JWT_KEY", random_key)
+
+    with open(tmp_dir.joinpath("main.py"), "w") as dest, \
+         open (origin_dir.joinpath("main.py")) as original:
+            for l in original:
+                dest.write(l.replace("#KEY_TO_BE_REPLACED#", key))
+
+    # Installing the dependencies, maybe could be done better?
+    os.chdir(tmp_dir)
+    os.system("pip install -r requirements.txt --target .")
+
+    return tmp_dir
